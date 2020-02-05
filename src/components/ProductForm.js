ffd090932249a535263bda475d5b21f747882a69
Ogunsola oyindamola <honeyspringworld@gmail.com>
2020-02-05 22:58:55 +0100

    create static page

---
+++ src/components/ProductForm.js
@@ -0,0 +1,10 @@
+import React,{Component } from 'react';
+
+class ProductForm extends Component {
+  render() {
+    return (
+    );
+  }
+}
+
+export default ProductForm;
---
+++ src/components/ProductTableHeader.js
@@ -0,0 +1,10 @@
+import React,{Component} from 'react';
+
+class ProductTableHeader Component {
+  render() {
+    return(
+    );
+  }
+}
+
+export default ProductTableHeader;
