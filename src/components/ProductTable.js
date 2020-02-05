8f876c453532927f0f005d75eee4d284fcb1c823
Ogunsola oyindamola <honeyspringworld@gmail.com>
2020-02-05 23:04:16 +0100

    pass a 'column'  prop  to the  ProductTableHeader  component

--- src/components/ProductTable.js
+++ src/components/ProductTable.js
@@ -4,10 +4,26 @@ import ProductTableHeader from './ProductTableHeader.js';
 
 class ProductTable extends React.Component {
   render() {
+      let productsAsArray = Object.keys(this.props.products).map((pid) => this.props.products[pid]);
+    let rows = [];
+
+    productsAsArray.forEach((product) => {
+      rows.push(
+        <ProductRow product={product} key={product.id} ></ProductRow>
+      );
+    });
+
     return (
       <div>
-        <ProductTableHeader ></ProductTableHeader>
-        <ProductRow ></ProductRow>
+        <table>
+          <thead>
+            <tr>
+              <ProductTableHeader column="name" ></ProductTableHeader>
+              <ProductTableHeader column="price" ></ProductTableHeader>
+            </tr>
+          </thead>
+          <tbody>{rows}</tbody>
+        </table>
       </div>
     );
   }
